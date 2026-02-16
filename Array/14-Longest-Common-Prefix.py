fa3243306e9cccb7a23cc75585c811acd4efda13
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-02-16 14:48:04 -0800

    add: solve 014 Longest Common Prefix using character-by-character scan

---
+++ Array/14-Longest-Common-Prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        first = strs[0]
+
+        for i in range(len(first)):
+            ch = first[i]
+            for s in strs[1:]:
+                if i >= len(s) or s[i] != ch:
+                    return first[:i]
+
+        return first
