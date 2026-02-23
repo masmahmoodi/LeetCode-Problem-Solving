cf60ec6a82025f4fb6a73a6c635f21e6dc9315ef
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-02-22 23:58:10 -0800

    solve: 268. Missing Number

---
+++ Array/268-Missing-Number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        return sum(range(n + 1)) - sum(nums)
